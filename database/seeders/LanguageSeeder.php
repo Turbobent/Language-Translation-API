bcb7cc56d6c90ad82fd2e1b909e58701c09eb961
Turbobent <kasp728h@edu.mercantec.dk>
2025-01-08 13:51:01 +0100

    changes

---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Language;
+use Illuminate\Database\Seeder;
+
+class LanguageSeeder extends Seeder
+{
+    public function run()
+    {
+        $languages = [
+            ['name' => 'English', 'code' => 'en'],
+            ['name' => 'Spanish', 'code' => 'es'],
+            ['name' => 'French', 'code' => 'fr'],
+            ['name' => 'German', 'code' => 'de'],
+        ];
+
+        Language::insert($languages);
+    }
+}
+
