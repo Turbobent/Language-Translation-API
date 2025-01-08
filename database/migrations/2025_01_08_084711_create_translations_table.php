2adca8b784911de3a688b89a12fc5c61f770d5da
Turbobent <kasp728h@edu.mercantec.dk>
2025-01-08 13:49:14 +0100

    migration

--- database/migrations/2025_01_08_084711_create_translations_table.php
+++ database/migrations/2025_01_08_084711_create_translations_table.php
@@ -9,13 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('translations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('source_language_id');
+            $table->unsignedBigInteger('target_language_id');
+            $table->text('source_text');
+            $table->text('translated_text');
             $table->timestamps();
+
+            $table->foreign('source_language_id')->references('id')->on('languages')->onDelete('cascade');
+            $table->foreign('target_language_id')->references('id')->on('languages')->onDelete('cascade');
         });
     }
+
 
     /**
      * Reverse the migrations.
