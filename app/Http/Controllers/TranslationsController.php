3062b2ab62ab87d57483253537d3912988127ddc
Turbobent <kasp728h@edu.mercantec.dk>
2025-01-08 09:47:46 +0100

    made all the nesseary files for my models

---
+++ app/Http/Controllers/TranslationsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Translations;
+use App\Http\Requests\StoreTranslationsRequest;
+use App\Http\Requests\UpdateTranslationsRequest;
+
+class TranslationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTranslationsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Translations $translations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Translations $translations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTranslationsRequest $request, Translations $translations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Translations $translations)
+    {
+        //
+    }
+}
